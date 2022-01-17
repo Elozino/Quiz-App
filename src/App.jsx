ebf129558c2a6a9ce453c260becae8c692a50db6
Elozino A. Ovedhe <ovedhee@gmail.com>
2022-01-17 15:30:45 +0100

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,35 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import "./index.css";
+
+import { money } from "./data";
+import Trivia from "./components/Trivia";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [quesNumber, setQuesNumber] = useState(4);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <div className="main">
+        <div className="top">
+          <div className="timer">30 </div>
+        </div>
+        <div className="bottom">
+          <Trivia />
+        </div>
+      </div>
+
+      <div className="prize">
+        <ul className="prizeList">
+          {money.map((item) => (
+            <li className={quesNumber === item.id ? "money active" : "money"}>
+              <span className="moneyNumber">{item.id}</span>
+              <span className="moneyAmount">{item.amount}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Trivia.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Trivia() {
+    return (
+      <div className="trivia">
+        <div className="question">Question</div>
+        <div className="answers">
+          <div className="answer">Answer</div>
+          <div className="answer">Answer</div>
+          <div className="answer">Answer</div>
+          <div className="answer">Answer</div>
+        </div>
+      </div>
+    );
+}
+
+export default Trivia
