7da554bfab802174d7e4f5aae85f42c28b9c25ed
Elozino A. Ovedhe <ovedhee@gmail.com>
2022-01-18 02:24:50 +0100

    quest & ans

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./index.css";
 
 import { money } from "./data";
 import Trivia from "./components/Trivia";
 
 function App() {
-  const [quesNumber, setQuesNumber] = useState(4);
+  const [quesNumber, setQuesNumber] = useState(1);
+  const [countDown, setCountDown] = useState(false);
+  const [questionFetched, setQuestionFetched] = useState("");
+
+
+  useEffect(() => {
+    fetch(
+      "https://opentdb.com/api.php?amount=15&category=9&difficulty=easy&type=multiple"
+    )
+      .then((res) => res.json())
+      .then((result) => setQuestionFetched(result.results[quesNumber - 1]));
+  }, []);
+
+  console.log(questionFetched);
 
   return (
     <div className="App">
@@ -14,14 +27,17 @@ function App() {
           <div className="timer">30 </div>
         </div>
         <div className="bottom">
-          <Trivia />
+          <Trivia questionProps={questionFetched} />
         </div>
       </div>
 
       <div className="prize">
         <ul className="prizeList">
           {money.map((item) => (
-            <li className={quesNumber === item.id ? "money active" : "money"}>
+            <li
+              key={item.id}
+              className={quesNumber === item.id ? "money active" : "money"}
+            >
               <span className="moneyNumber">{item.id}</span>
               <span className="moneyAmount">{item.amount}</span>
             </li>
--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -1,17 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
 
-function Trivia() {
-    return (
-      <div className="trivia">
-        <div className="question">Question</div>
-        <div className="answers">
-          <div className="answer">Answer</div>
-          <div className="answer">Answer</div>
-          <div className="answer">Answer</div>
-          <div className="answer">Answer</div>
-        </div>
+function Trivia({ countDown, questNumber, questionProps }) {
+  const [selectedAnswer, setSelectedAnswer] = useState("");
+  const [className, setClassName] = useState("answer");
+  const answer = [
+    questionProps.correct_answer,
+    questionProps.incorrect_answers,
+  ].flat();
+
+  console.log(answer);
+
+  const handleClick = (ans) => {
+    setSelectedAnswer(ans);
+    setClassName("answer active");
+  };
+
+  return (
+    <div className="trivia">
+      <div className="question">{questionProps?.question}</div>
+      <div className="answers">
+        {answer?.map((ans) => (
+          <div
+            key={ans}
+            className={selectedAnswer === ans ? className : "answer"}
+            onClick={() => handleClick(ans)}
+          >
+            {ans}
+          </div>
+        ))}
       </div>
-    );
+    </div>
+  );
 }
 
-export default Trivia
+export default Trivia;
